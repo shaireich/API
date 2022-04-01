1d0d896f73fed1b9fb27c4a32627d3b926f9b6ae
shaireich <93054916+shaireich@users.noreply.github.com>
2022-04-01 15:53:35 +0300

    Created
    Those 2 Files Are The Only Files In Project Kind Of Consule Application, which sending Get Api Request For GitHub Api And Execute Deserialization Of Json Response In Order To Show In Output Part Of The Information.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using ConsoleAppApiJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+/**/using System.Net.Http;
+/**/using System.Net.Http.Headers;
+/**/using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace ApiJson
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            //var msg = await stringTask;
+            //Console.Write(msg);
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.Name);
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories2()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            //var msg = await stringTask;
+            //Console.Write(msg);
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            return repositories;
+        }
+
+        [STAThread]
+        static async Task Main(string[] args)
+        {
+            //await ProcessRepositories();
+            var repositories = await ProcessRepositories2();
+
+            //foreach (var repo in repositories)
+            //    Console.WriteLine(repo.Name);
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine();
+
+                Console.WriteLine(repo.LastPush);
+            }
+        }
+
+
+        //    static void Main()
+        //    {
+        //        Application.EnableVisualStyles();
+        //        Application.SetCompatibleTextRenderingDefault(false);
+        //        Application.Run(new Form1());
+        //    }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/**/using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleAppApiJson
+{
+    
+    internal class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        /// <summary>
+        /// for date in my time zone
+        /// </summary>
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    }
+}
